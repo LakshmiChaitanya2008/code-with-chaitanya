85d1304d2e81504c92eac0cc2a6fdfd1ade723e4
LakshmiChaitanya2008 <lakshmichaitanyachalamalasetti@gmail.com>
2021-09-05 14:07:36 +0530

    feat: pwa test 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ["nuxt-vite"],
+  buildModules: ["nuxt-vite", "@nuxtjs/pwa"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
